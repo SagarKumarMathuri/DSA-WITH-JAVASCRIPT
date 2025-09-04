b0451652817acce48f396f5d5ce2087a09ecb7f7
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-05 01:50:43 +0530

    add some file

--- Array.js
+++ Array.js
@@ -8,3 +8,43 @@ const firstElement = arr[0];
 const arrLength = arr.length;
 const lastElement = arr[arrLength - 1];
 console.log(firstElement, arrLength, lastElement);
+
+// output: 1 8 [1, 2, 3]
+
+// how do you remove the last element of an array?
+const lastElement1 = arr.pop();
+console.log(arr, lastElement1);
+// output: [1, 2, 3, 4, 5, "hello", { name: "John" }] [1, 2, 3]
+
+// how do you add an element to the end of an array?
+arr.push(5);
+console.log(arr);
+// output: [1, 2, 3, 4, 5, "hello", { name: "John" }, 5]
+
+// how do you add an element to the beginning of an array?
+
+arr.unshift(0);
+console.log(arr);
+// output: [0, 1, 2, 3, 4, 5, "hello", { name: "John" }, 5]
+
+// how do you remove the first element of an array?
+
+arr.shift();
+console.log(arr);
+// output: [1, 2, 3, 4, 5, "hello", { name: "John" }, 5]
+
+// how do you loop through an array?
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+// output: 1 2 3 4 5 "hello" { name: "John" } 5
+
+arr.forEach((element) => {
+  console.log(element);
+});
+// output: 1 2 3 4 5 "hello" { name: "John" } 5
+
+for (const element of arr) {
+  console.log(element);
+}
+// output: 1 2 3 4 5 "hello" { name: "John" } 5
