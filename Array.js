0b0fde1fd79623037acdac2ff428f68b25b71263
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-04 11:50:41 +0530

    some changes arra file

--- Array.js
+++ Array.js
@@ -1,7 +1,10 @@
 // how do you create an empty array?
 const arr = [1, 2, 3, 4, 5, "hello", { name: "John" }, [1, 2, 3]];
 console.log(arr);
+// output: [1, 2, 3, 4, 5, "hello", { name: "John" }, [1, 2, 3]]
 
 // how do you access the first and last element of an array?'
-console.log(arr[0]);
+const firstElement = arr[0];
 const arrLength = arr.length;
+const lastElement = arr[arrLength - 1];
+console.log(firstElement, arrLength, lastElement);
