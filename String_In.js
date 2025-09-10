3c08dcb52778fc2568a1e3b163abb41c5b0d2d72
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-10 10:20:40 +0530

    Add string file

--- String_In.js
+++ String_In.js
@@ -7,7 +7,8 @@
 // console.log(str.slice(-1)); // output: Hello
 // console.log(str.substring(3, 6));
 // console.log(str.replace("World", "JavaScript")); // output: Hello, JavaScript!
+// console.log(str.replaceAll("Hello", "Hi")); // output: Hi Hi, World!
 
 let str = "Hello Hello, World!";
-
-console.log(str.replaceAll("Hello", "Hi")); // output: Hi Hi, World!
+console.log(str.toUpperCase()); // output: Hello, World!
+console.log(str.toLowerCase()); // output: hello, world!
