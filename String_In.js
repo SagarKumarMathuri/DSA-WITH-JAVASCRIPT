c1f5fe0548b79c993d9415f2c226fd29e406e4cb
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-10 10:17:38 +0530

    Add string file

--- String_In.js
+++ String_In.js
@@ -6,5 +6,8 @@
 //console.log(str.length); // output: 13
 // console.log(str.slice(-1)); // output: Hello
 // console.log(str.substring(3, 6));
+// console.log(str.replace("World", "JavaScript")); // output: Hello, JavaScript!
 
-let str = "Hello, World!";
+let str = "Hello Hello, World!";
+
+console.log(str.replaceAll("Hello", "Hi")); // output: Hi Hi, World!
