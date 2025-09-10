49bb50acb626afeeae6a1a9ea0a8d1f21e1c1d61
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-10 10:11:44 +0530

    Add string file

--- String_In.js
+++ String_In.js
@@ -1,7 +1,10 @@
 // string in javascript
 
-let str = "Hello, World!";
-console.log(str); // output: Hello, World!
+//console.log(str); // output: Hello, World!
 
 // how do you get the length of a string?
-console.log(str.length); // output: 13
+//console.log(str.length); // output: 13
+// console.log(str.slice(-1)); // output: Hello
+// console.log(str.substring(3, 6));
+
+let str = "Hello, World!";
