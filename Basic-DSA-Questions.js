7e49686600f157920d56d8dfc3a47c235c08ecd5
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-13 11:59:04 +0530

    Did some changes

--- Basic-DSA-Questions.js
+++ Basic-DSA-Questions.js
@@ -1,3 +1,9 @@
 // use console.log to display the current date and time
 const newDate = new Date();
 console.log(newDate.getFullYear());
+
+// create two variables and assign them values
+let a = "sagar";
+let b = "kumar";
+
+console.log(a + " " + b);
