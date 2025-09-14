c9ef807cbe53a2cd0e19b04275ad8ec6c8320400
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-14 12:53:57 +0530

    add 2d array

--- 2D-Arrays.js
+++ 2D-Arrays.js
@@ -1,11 +1,24 @@
-arr = [
-  ["a", "b", "c"],
-  ["d", "e", "f"],
-  ["g", "h", "i"],
+// arr = [
+//   ["a", "b", "c"],
+//   ["d", "e", "f"],
+//   ["g", "h", "i"],
+// ];
+// for (let i = 0; i < arr.length; i++) {
+//   let row = arr[i];
+//   for (let j = 0; j < row.length; j++) {
+//     console.log(row[j]);
+//   }
+// }
+
+let mat = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
 ];
-for (let i = 0; i < arr.length; i++) {
-  let row = arr[i];
-  for (let j = 0; j < row.length; j++) {
-    console.log(row[j]);
+for (let i = 0; i < mat.length; i++) {
+  for (let j = 0; j < mat[i].length; j++) {
+    if (i === j) {
+      console.log(mat[i][j]);
+    }
   }
 }
