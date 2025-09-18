12734871756f77c849dd12285a6e56d7ce901faf
sagar kumar mathuri <sagarku2002@gmail.com>
2025-09-18 11:20:57 +0530

    file

---
+++ recursion.js
@@ -0,0 +1,10 @@
+function greet() {
+  console.log("Hello, World!");
+}
+
+function enjoy(params) {
+  console.log("I am enjoying learning JavaScript!");
+}
+function temp(params) {
+  console.log("kuch kar rahe ho?");
+}
