7317078491d90425a20c46ccb7a5dc0efff06b80
sagar kumar mathuri <sagarku2002@gmail.com>
2026-01-05 10:16:09 +0530

    Initial changes

--- App.js
+++ App.js
@@ -4,3 +4,19 @@
 // process.stdout.write("Application is running...");
 // process.stdout.write("No errors detected.");
 // process.stdout.write("All systems operational.");
+
+// let prompt = require("prompt-sync")();
+// let a = prompt("Enter first number: ");
+// process.stdout.write(a);
+
+let prompt = require("prompt-sync")();
+let n = prompt("Enter first number: ");
+for(let i=0; i<n; i++){
+  for(let j=0; j<n; j++){
+   process.stdout.write("* ");
+  } 
+    console.log(" ");
+}
+
+
+
