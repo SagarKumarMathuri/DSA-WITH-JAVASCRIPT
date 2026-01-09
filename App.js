ce0d15783a1f9ba3aaa46c6a478247851d5fee44
sagar kumar mathuri <sagarku2002@gmail.com>
2026-01-10 00:56:30 +0530

    Initial commit

--- App.js
+++ App.js
@@ -9,14 +9,58 @@
 // let a = prompt("Enter first number: ");
 // process.stdout.write(a);
 
+// let prompt = require("prompt-sync")();
+// let n = prompt("Enter first number: ");
+// for(let i=0; i<n; i++){
+//   for(let j=0; j<i; j++){
+//    process.stdout.write(j+" ");
+//   }
+//     console.log(" ");
+// }
+
+// let prompt = require("prompt-sync")();
+// let n = prompt("Enter first number: ");
+// for(let i=1; i<=n; i++){
+
+//   for(let j=1; j<=n-i+1; j++){
+//    process.stdout.write("  ");
+//   }
+//   for(let j=1; j<=i; j++){
+//     process.stdout.write("* ");
+//   }
+//     console.log();
+// }
+
+// let prompt = require("prompt-sync")();
+// let n = prompt("Enter first number: ");
+// for(let i=0; i<n; i++){
+//   let  ascii = 65;
+//   for(let j=0; j<i; j++){
+//    process.stdout.write(String.fromCharCode(ascii)+" ");
+//     ascii++;
+//   }
+//     console.log(" ");
+// }
+
+// let prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter first number: "));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if (i == j || i + j == n + 1) {
+//       process.stdout.write("* ");
+//     } else process.stdout.write("  ");
+//   }
+
+//   console.log();
+// }
+
 let prompt = require("prompt-sync")();
-let n = prompt("Enter first number: ");
-for(let i=0; i<n; i++){
-  for(let j=0; j<n; j++){
-   process.stdout.write("* ");
-  } 
-    console.log(" ");
+let n = Number(prompt("Enter first number: "));
+
+if(n > 0){
+  let fact = 1;
+  for(let i = 1; i <= n; i++){
+     fact *= i;
+  }
+  console.log(fact);
 }
-
-
-
