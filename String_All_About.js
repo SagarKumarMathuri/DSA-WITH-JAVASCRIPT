dcc6b4d569a20060bcef4d1f3979e1986fe5c7cf
sagar kumar mathuri <sagarku2002@gmail.com>
2026-02-07 12:13:21 +0530

    Some

--- String_All_About.js
+++ String_All_About.js
@@ -1,2 +1,14 @@
+// let s = "sagar"
+// console.log(s.slice(-3, s.length))
+
+// let s = "sagar"
+// console.log(s.slice(1,4))
+
+
 let s = "sagar"
-console.log(s.slice(-3, s.length))
+console.log(s.slice(-3,s.length))
+
+
+
+
+
